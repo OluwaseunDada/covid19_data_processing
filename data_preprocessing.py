e4e09265c967660ed5445d2d24facc66421d55cf
Alexander Dada <degreat.dada@gmail.com>
2020-05-28 12:31:05 +0000

    adding feature to generate one result.json file per month as per Yazid's request

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -8,7 +8,6 @@ def read_tweeter_file(tweets_file):
     file1 = open(tweets_file, 'r')
     Lines = file1.readlines()
     r = Lines[2]
-    print(r)
     my_list = []
     for one_line in Lines:
         mydict = json.loads(one_line)
@@ -35,21 +34,39 @@ def read_tweeter_file(tweets_file):
     return my_list
 
 
-
 # loop through tweet_files in the folder
+
+all_files_in_folder = []
+flag_set = set()
 for filename in os.listdir(directory):
     if filename.endswith(".jsonl"):
-        tweets_file = os.path.join(directory, filename)
-        print("tweets_file: " + tweets_file)
+         tweets_file = os.path.join(directory, filename)
+         all_files_in_folder.append(tweets_file)
 
-        thelist = read_tweeter_file(tweets_file)
+         f = Path(tweets_file).stem     # the original filename e.g. coronavirus-tweet-id-2020-01-31-23
+         flag = f[:-3] # drop the last 3 characters e.g. coronavirus-tweet-id-2020-01-31
+         flag_set.add(flag)
 
-        # Serializing json
-        json_object = json.dumps(thelist, indent = 4)
 
-        # Writing the results
-        f = Path(tweets_file).stem     # the original filename e.g. coronavirus-tweet-id-2020-01-31-23
-        result_file = f.replace("coronavirus", "result") # result-tweet-id-2020-01-31-23
-        print("result_file: " + result_file)
-        with open(result_file, "w") as outfile:
-            outfile.write(json_object)
+# loop through the flagset and fetch corresponding files
+for x in flag_set:
+    # from all files in the folder, find those matching the flag
+    flag_list = []
+    for aFile in all_files_in_folder:
+        if x in aFile:
+            someList = read_tweeter_file(aFile)
+            flag_list.append(someList)
+        else:
+            continue
+
+    # for each date, write the content of the flag_list to a file
+    json_object = json.dumps(flag_list, indent=4)  # Serializing json
+
+    # Writing the results
+    result_file = x.replace("coronavirus", "result")  # result-tweet-id-2020-01-31
+    with open(result_file, "w") as outfile:
+        outfile.write(json_object)
+
+
+
+
